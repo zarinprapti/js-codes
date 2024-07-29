4d0b644786fbdd0a942c086d211e57cd6afea934
Zarin Rafa Prapti <zarinprapti044@gmail.com>
2024-07-30 02:58:56 +0600

    js codes added

---
+++ challenge.js
@@ -0,0 +1,26 @@
+// let myName = "Zarinrafaprapti";
+// let nameLength = myName.length;
+// console.log(nameLength);
+// console.log(myName.toUpperCase());
+
+let myAge = 30;
+// let stringAge = myAge.toString();
+// console.log(stringAge);
+// console.log(typeof(stringAge));
+
+// let num = '123';
+// console.log(num);
+// console.log(typeof(num));
+
+// console.log(myName.indexOf('i'));
+
+// let myNumber = "30";
+// let a = Number(myNumber);
+// let sum = a + 40
+// console.log(sum);
+
+let firstName = "Zarin Rafa";
+let lastName = "Prapti";
+console.log(firstName + ' ' + lastName);
+
+console.log(`I'm ${myAge} years old`);
